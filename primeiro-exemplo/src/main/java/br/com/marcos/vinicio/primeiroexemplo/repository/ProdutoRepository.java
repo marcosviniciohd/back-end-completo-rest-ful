d18029f9a82e19090b19ddb99c18e5726a7c1955
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2023-01-19 16:48:37 -0300

    Projeto iniciado, criado Produto.class e ProdutoRepository.class

---
+++ primeiro-exemplo/src/main/java/br/com/marcos/vinicio/primeiroexemplo/repository/ProdutoRepository.java
@@ -0,0 +1,53 @@
+package br.com.marcos.vinicio.primeiroexemplo.repository;
+
+import br.com.marcos.vinicio.primeiroexemplo.model.Produto;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProdutoRepository {
+    private List<Produto> produtos = new ArrayList<>();
+    private Integer ultimoId = 0;
+
+    /**
+     * Método para retornar uma lista de produtos.
+     * @return Lista de produtos
+     */
+    public List<Produto> obterTodos(){
+        return produtos;
+    }
+
+    /**
+     * Método que retorna um produto encontrado pelo seu Id.
+     * @param id do produto que será localizado.
+     * @return Retorna um produto caso seja encontrado.
+     */
+    public Optional<Produto> obterPorId(Integer id){
+        return produtos.stream().filter(produto -> produto.getId() == id).findFirst();
+    }
+
+    /**
+     * Método para adicionar produto na lista.
+     * @param produto que será adicionado
+     * @return Retorna o produto que foi adicionado na lista.
+     */
+    public Produto adicionar(Produto produto){
+        ultimoId++;
+
+        produto.setId(ultimoId);
+        produtos.add(produto);
+
+        return produto;
+    }
+
+    /**
+     * Método para deletar o produto por id.
+     * @param id do produto a ser deletado.
+     */
+    public void deletar(Integer id){
+        produtos.removeIf(produto -> produto.getId() == id);
+    }
+}
---
+++ primeiro-exemplo/src/test/java/br/com/marcos/vinicio/primeiroexemplo/PrimeiroExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.vinicio.primeiroexemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
